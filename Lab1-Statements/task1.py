fb4755196c556b73e33389b310c378dec70e52dc
Reigh Kemp <rkemp1@murraystate.edu>
2025-08-25 11:23:04 -0500

    done with task 1

---
+++ Lab1-Statements/task1.py
@@ -0,0 +1,7 @@
+name = input('Waht is your name? ')
+print('Hello,', name)
+
+first_name = input('Enter First name')
+last_name = input(' Enter Last name')
+greeting = 'Hello, ' + first_name + last_name
+print(greeting)
---
+++ Lab1-Statements/task2.py
@@ -0,0 +1,3 @@
+quantity = int(input('Enter number of bags'))
+price = float(input( 'Enter price per bag'))
+print('Total cost:', quantity*price)
