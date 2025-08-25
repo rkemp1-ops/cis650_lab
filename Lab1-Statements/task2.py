9fcd2d310c67e2110c18060fff19e010f83b198e
Reigh Kemp <rkemp1@murraystate.edu>
2025-08-25 11:30:55 -0500

    Done with task 2

--- Lab1-Statements/task2.py
+++ Lab1-Statements/task2.py
@@ -1,3 +1,7 @@
 quantity = int(input('Enter number of bags'))
 price = float(input( 'Enter price per bag'))
 print('Total cost:', quantity*price)
+miles_driven = int( input( 'enter Miles Driven'))
+gallons_used = float( input( 'enter gallons used'))
+mpg= miles_driven / gallons_used
+print('Miles per gallon:', mpg)
